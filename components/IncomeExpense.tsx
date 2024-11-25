e537831d041ef0c885fdd01d7c177f1035b9ec10
saatvik-10 <saatvikmadan529@gmail.com>
2024-11-25 14:06:29 +0100

    updated balance component

--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,4 +1,5 @@
 import getIncExp from '@/app/actions/getIncExp';
+import { formatAmount } from '@/lib/utils';
 
 const IncomeExpense = async () => {
   const { income, expense } = await getIncExp();
@@ -7,11 +8,15 @@ const IncomeExpense = async () => {
     <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
-        <p className='money plus'>₹ {income}</p>
+        <p className='money plus'>
+          ₹ {formatAmount(Number(income?.toFixed(2)))}
+        </p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className='money minus'>₹ {expense}</p>
+        <p className='money minus'>
+          ₹ {formatAmount(Number(expense?.toFixed(2)))}
+        </p>
       </div>
     </div>
   );
